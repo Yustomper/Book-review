c4be3dd312fe8913c3aa64627781bfc82acedfa5
yust <yustminator@gmail.com>
2024-07-27 17:42:42 -0500

    please

--- Book_Review/settings.py
+++ Book_Review/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
