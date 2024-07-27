9955d8f05c577c1219fe7ff002544c1216ac2ed1
yust <yustminator@gmail.com>
2024-07-27 17:51:31 -0500

    intentando arreglar img

--- Book_Review/settings.py
+++ Book_Review/settings.py
@@ -148,8 +148,7 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/login/'
 
