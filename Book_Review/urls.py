f3a879d8766973b32a12b7f06de858e34193c9dc
yust <yustminator@gmail.com>
2024-07-27 17:17:20 -0500

    agregando staticfiles para despliegue

--- Book_Review/urls.py
+++ Book_Review/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +30,5 @@ urlpatterns = [
     path('search/',include('search.urls'))
 ]
 if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
