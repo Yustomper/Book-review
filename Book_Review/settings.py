3ce895f0a6099403843ff9a5370fa92c845b686d
yust <yustminator@gmail.com>
2024-07-27 17:04:24 -0500

    postgresql

--- Book_Review/settings.py
+++ Book_Review/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'Book_Review.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
-
 
 
 # Password validation
